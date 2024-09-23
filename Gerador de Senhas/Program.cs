4ab84226ad5ec16471a2969a9f29f97a9a05d631
leonardoheringer <heringerleonardo560@gmail.com>
2024-09-23 15:01:00 -0300

    Gerador Senhas e Lançador Dado

---
+++ Gerador de Senhas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class GeradorDeCaracteres
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao gerador de senhas aleatórias!");
+
+        Console.Write("Digite o número de caracteres da senha que deseja gerar: ");
+        int quantidade;
+        while (!int.TryParse(Console.ReadLine(), out quantidade) || quantidade <= 0)
+        {
+            Console.Write("Por favor, insira um número válido: ");
+        }
+
+        string senhaAleatoria = GerarsenhaAleatoria(quantidade);
+        Console.WriteLine($"Senha gerada: {senhaAleatoria}");
+    }
+
+    static string GerarsenhaAleatoria(int quantidade)
+    {
+        const string caracteresPermitidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        char[] caracteres = new char[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            int indice = random.Next(caracteresPermitidos.Length);
+            caracteres[i] = caracteresPermitidos[indice];
+        }
+
+        return new string(caracteres);
+    }
+}
---
+++ Simulador de Lançamento de Dados/Program.cs
@@ -0,0 +1,28 @@
+﻿
+		Random random = new Random();
+		bool rolardado = true;
+		Console.Write("Bem-vindo ao lançador de dado");
+		Console.WriteLine("\nDeseja jogar ?");
+		string escolha = Console.ReadLine().ToLower();
+		if (escolha == "não")
+		{
+			rolardado = false;
+			Console.WriteLine("OK!");
+		}
+
+		while (rolardado)
+		{
+			Random randNum = new Random();
+			int dado = randNum.Next(1, 6);
+			Console.Write($"O dado caiu no numero {dado} \n");
+			Console.WriteLine("Deseja jogar novamente?");
+			string escolha1 = Console.ReadLine().ToLower();
+			if (escolha1 == "não")
+			{
+				rolardado = false;
+				Console.WriteLine("OK!");
+			}
+
+			Console.WriteLine();
+		}
+	
